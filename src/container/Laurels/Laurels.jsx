58bd0181103a02845266bd54070ea57e6369fd54
Aryan <69583042+aryannxroot@users.noreply.github.com>
2024-06-28 15:29:35 +0530

    Added Laurels

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg section__padding flex__center'>
+    <div className='app__laurels'>
+      <div className='app__laurels-awards'>
+        <SubHeading title ="Awards & recognition"/>
+        <h1 className='headtext__cormorant'>Our Laurels</h1>
+        <div className='app__laurels-awards__boxes'>
+          <div className='app__laurels-awards__box'>
+              <img src={images.award02} alt="award" />
+              <div>
+                <p className='p__cormorant'>Bib Gourmond</p>
+                <p classname="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur.</p>
+              </div>
+          </div>
+          <div className='app__laurels-awards__box'>
+              <img src={images.award01} alt="award" />
+              <div>
+                <p className='p__cormorant'>Rising Star</p>
+                <p classname="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur.</p>
+              </div>
+          </div>
+          <div className='app__laurels-awards__box'>
+              <img src={images.award05} alt="award" />
+              <div>
+                <p className='p__cormorant'>AA Hospitality</p>
+                <p classname="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur.</p>
+              </div>
+          </div>
+          <div className='app__laurels-awards__box'>
+              <img src={images.award03} alt="award" />
+              <div>
+                <p className='p__cormorant'>Outstanding Chef</p>
+                <p classname="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className='app__laurels-img'>
+        <img src={images.laurels} alt=""/>
+      </div>
+    </div>
   </div>
 );
 
